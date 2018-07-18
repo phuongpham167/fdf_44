3df39193902cff8fbcd0855e503af69c7d194b2f
nguyenhongphuc2018 <nguyen.hong.phuc@framgia.com>
2018-07-18 16:28:54 +0700

    show product

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def load_new_product product
+    if product.new_product?
+      render :partial => "shared/new_product", :locals => { :product => product } 
+    end
+  end
 end
